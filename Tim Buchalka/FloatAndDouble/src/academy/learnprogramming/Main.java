43e849f8165efebeab2e858a2cfb33c958bcf4cb
Daniel <daniel_ys1@outlook.com>
2022-12-07 22:08:40 -0600

    first commit

---
+++ Tim Buchalka/FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00; // more faster
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        // challenge convert a given number of pounds to kilograms
+        double numberOfPounds = 200d;
+        double convertedKg = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + convertedKg);
+
+    }
+}
